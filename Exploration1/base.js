a0c631e016190b6893b743c01b8a1d25eef33a39
Hunter Ginther <hagd5d@mail.missouri.edu>
2017-09-14 07:57:17 -0500

    Exploration Nav Initial

---
+++ Exploration1/base.js
@@ -0,0 +1,44 @@
+function stageContent(content){
+  $("#stage").html(content);
+}
+
+function evaluatePath(path){
+  // clean up the endpoint
+  var request = path.split("/").pop();
+
+  // request content
+  if (request == "ReactTutorial") {
+    $.get("ReactTutorial/index.html", stageContent);
+  }
+  // else if (request == "contact") {
+  //   $.get("contact.html", stageContent);
+  // }
+  else {
+    $.get("index.html", stageContent);
+  }
+}
+
+$(function() {
+  // evaluate path to start
+  evaluatePath(location.pathname);
+
+  // when a link is clicked ..
+  $("nav[role=navigation] a").click(function(e){
+    // don't follow href
+    e.preventDefault();
+
+    // instead remember href
+    var request = $(this).attr("href");
+
+    // add href to the browser's history
+    history.pushState(null, null, request);
+
+    // then evaluate it
+    evaluatePath(request);
+  });
+
+  // If the back button is pressed ..
+  $(window).on("popstate", function() {
+    evaluatePath(location.pathname);
+  });
+});
